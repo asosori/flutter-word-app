c780ab0726703f5d8d0841b7d635b0342f88360b
asosori <tsukimiteritama@gmail.com>
2020-02-11 14:56:50 +0900

    create the base of word list page ui

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,24 @@ class MyApp extends StatelessWidget {
         accentColor: const Color(0xFF2196f3),
         canvasColor: const Color(0xFFe7e3f5),
       ),
-      home: MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WordCreateScreen(),
+        '/list': (context) => WordListScreen(),
+        //'/edit': (context) => WordEditScreen()
+      },
+      //home: WordCreateScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+class WordCreateScreen extends StatefulWidget {
+  WordCreateScreen({Key key}) : super(key: key);
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _WordCreateScreenState createState() => _WordCreateScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _WordCreateScreenState extends State<WordCreateScreen> {
   final controller = TextEditingController();
   int _index = 0;
 
@@ -35,14 +41,11 @@ class _MyHomePageState extends State<MyHomePage> {
     var container = SingleChildScrollView(
       child:
         Container(
-          width: 500,
-          //color: Colors.blue,
           child: Card(
             margin: EdgeInsets.only(top: 40, left: 60, right: 60),
             color: Colors.white,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Container(
@@ -135,6 +138,13 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: Icon(Icons.list)
             )
           ],
+          onTap: (int value){
+            if (value == 1)
+              Navigator.pushNamed(
+                context,
+                '/list'
+              );
+          },
         ),
       )
     );
@@ -142,5 +152,182 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void createWord(){
     print("ボタンが押されました");
+  }
+}
+
+class WordListScreen extends StatefulWidget {
+  WordListScreen({Key key}) : super(key: key);
+  @override
+  _WordListScreenState createState() => _WordListScreenState();
+}
+
+class _WordListScreenState extends State<WordListScreen> {
+  int _index = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    var container = SingleChildScrollView(
+      child:
+        Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 40)),
+            Container(
+              color: Colors.red,
+              child: Card(
+                margin: EdgeInsets.only(bottom: 40, left: 40, right: 40),
+                color: Colors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    LayoutBuilder(
+                      builder: (BuildContext context, BoxConstraints constraints) {
+                        return Row(
+                          children: <Widget>[
+                            Container(
+                              color: Colors.blue,
+                              width: constraints.maxWidth/2,
+                              child: Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.all(10),
+                                child: Text(
+                                  "問題",
+                                  style: TextStyle(fontSize: 22,
+                                  )                            
+                                ),
+                              )
+                            ),
+                            Container(
+                              color: Colors.green,
+                              width: constraints.maxWidth/2,
+                              child: Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.all(10),
+                                child: Text(
+                                  "解答",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white
+                                  )
+                                ),
+                              )
+                            ),
+                          ],
+                        );
+                      }
+                    ),
+                    Padding(padding: EdgeInsets.all(10),),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                      child: Text(
+                        'aaa',
+                        style: TextStyle(fontSize: 18),
+                        )
+                    ),
+                    Padding(padding: EdgeInsets.only(bottom: 30),),
+                  ]
+                )
+              )
+            ),
+            Container(
+              color: Colors.red,
+              child: Card(
+                margin: EdgeInsets.only(bottom: 40, left: 40, right: 40),
+                color: Colors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    LayoutBuilder(
+                      builder: (BuildContext context, BoxConstraints constraints) {
+                        return Row(
+                          children: <Widget>[
+                            Container(
+                              color: Colors.blue,
+                              width: constraints.maxWidth/2,
+                              child: Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.all(10),
+                                child: Text(
+                                  "問題",
+                                  style: TextStyle(fontSize: 22,
+                                  )                            
+                                ),
+                              )
+                            ),
+                            Container(
+                              color: Colors.green,
+                              width: constraints.maxWidth/2,
+                              child: Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.all(10),
+                                child: Text(
+                                  "解答",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white
+                                  )
+                                ),
+                              )
+                            ),
+                          ],
+                        );
+                      }
+                    ),
+                    Padding(padding: EdgeInsets.all(10),),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                      child: Text(
+                        'aaa',
+                        style: TextStyle(fontSize: 18),
+                        )
+                    ),
+                    Padding(padding: EdgeInsets.only(bottom: 30),),
+                  ]
+                )
+              )
+            )
+          ],
+        )
+    );
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('単語帳アプリ'),
+        centerTitle: true,
+        leading: BackButton(color: Colors.white,),
+        ),
+      body: container,
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.white,
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _index,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              title: Text("単語作成"),
+              icon: Icon(Icons.library_add),
+            ),
+            BottomNavigationBarItem(
+              title: Text("単語一覧"),
+              icon: Icon(Icons.list)
+            )
+          ],
+          onTap: (int value){
+            if (value == 0)
+              Navigator.pushNamed(
+                context,
+                '/'
+              );
+          },
+        ),
+      )
+    );
+  }
+
+  void createWord(){
+    print("ボタンが押されたよ");
   }
 }
